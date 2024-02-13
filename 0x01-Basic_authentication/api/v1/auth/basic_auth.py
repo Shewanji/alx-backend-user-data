b41022b2904581af1401211c86d0528b6d6e21cb
Shewanji <marrionwanjiru26@gmail.com>
2024-02-13 14:32:50 +0000

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -23,3 +23,15 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split(" ")[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Decodes the Base64-encoded part of an Authorization header."""
+        if not base64_authorization_header \
+                or not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            return base64.b64decode(base64_authorization_header.encode())\
+                .decode('utf-8')
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
