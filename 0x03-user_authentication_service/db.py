24495180638e744ed5cbd3e8bc91b9dea8d740bf
Shewanji <marrionwanjiru26@gmail.com>
2024-02-20 16:02:54 +0000

    code fix

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -45,10 +45,8 @@ class DB:
         """Find a user by filtering with input arguments"""
         try:
             user = self._session.query(User).filter_by(**kwargs).one()
-            if user is None:
-                raise NoResultFound()
-            return user
         except NoResultFound:
             raise NoResultFound()
-        except InvalidRequestError as e:
+        except InvalidRequestError:
             raise InvalidRequestError()
+        return user
