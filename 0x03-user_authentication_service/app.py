935e976a9914bea4836be040777018981a5e4a57
Shewanji <marrionwanjiru26@gmail.com>
2024-02-21 09:25:32 +0000

    Basic Flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""module for a basic Flask app"""
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def welcome():
+    """Return a welcome message"""
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
